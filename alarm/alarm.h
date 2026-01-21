6d9556244f63f83bd1af9c7135110a0301ff5255
mz-mittsuu <mizutani.mitsunobu.1022@gmail.com>
2026-01-21 23:13:50 +0900

    WIP

---
+++ alarm/alarm.h
@@ -0,0 +1,22 @@
+// alarm/alarm.h
+#pragma once
+#include <string>
+
+class Sensoralarm
+{
+public:
+    void updateSensortemp(int newsensorTemp);
+    int getLedStatus();
+    int getSensorTemp();
+private:
+    void ledStatusChange();
+    constexpr static char* LED_DIR = "/sys/class/leds/am62-sk:green:heartbeat/";
+    bool writeFile(const std::string& path, const std::string& value);
+    bool writeInt(const std::string& path, int v);
+    void setOff();
+    void setOn();
+    void blinkSlow();
+    void blinkFast();
+    int ledStatus = 0;
+    int sensorTemp = 20;
+};
