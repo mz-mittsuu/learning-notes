8a1633118c8a423b011fa14440897408a72ea881
mz-mittsuu <mizutani.mitsunobu.1022@gmail.com>
2026-01-22 00:20:37 +0900

    WIP

--- alarm/alarm.cpp
+++ alarm/alarm.cpp
@@ -3,61 +3,72 @@
 #include <fstream>
 #include <string>
 
-bool Sensoralarm::writeFile(const std::string& path, const std::string& value) {
+const std::string Sensoralarm::LED_DIR = "/sys/class/leds/am62-sk:green:heartbeat/";
+
+bool Sensoralarm::writeFile(const std::string& path, const std::string& value)
+{
     std::ofstream ofs(path);
     if (!ofs) return false;
     ofs << value;
     return bool(ofs);
 }
 
-bool Sensoralarm::writeInt(const std::string& path, int v) {
+bool Sensoralarm::writeInt(const std::string& path, int v)
+{
     return writeFile(path, std::to_string(v));
 }
 
-void Sensoralarm::setOff() {
-    writeFile(std::string(LED_DIR) + "trigger", "none");
-    writeInt(std::string(LED_DIR) + "brightness", 0);
+void Sensoralarm::setOff()
+{
+    writeFile(LED_DIR + "trigger", "none");
+    writeInt(LED_DIR + "brightness", 0);
 }
 
-void Sensoralarm::setOn() {
-    writeFile(std::string(LED_DIR) + "trigger", "none");
-    writeInt(std::string(LED_DIR) + "brightness", 1);
+void Sensoralarm::setOn()
+{
+    writeFile(LED_DIR + "trigger", "none");
+    writeInt(LED_DIR + "brightness", 1);
 }
 
-void Sensoralarm::blinkSlow() {
-    writeFile(std::string(LED_DIR) + "trigger", "timer");
-    writeInt(std::string(LED_DIR) + "delay_on", 200);
-    writeInt(std::string(LED_DIR) + "delay_off", 800);
+void Sensoralarm::blinkSlow()
+{
+    writeFile(LED_DIR + "trigger", "timer");
+    writeInt(LED_DIR + "delay_on", 200);
+    writeInt(LED_DIR + "delay_off", 800);
 }
 
-void Sensoralarm::blinkFast() {
-    writeFile(std::string(LED_DIR) + "trigger", "timer");
-    writeInt(std::string(LED_DIR) + "delay_on", 100);
-    writeInt(std::string(LED_DIR) + "delay_off", 100);
+void Sensoralarm::blinkFast()
+{
+    writeFile(LED_DIR + "trigger", "timer");
+    writeInt(LED_DIR + "delay_on", 100);
+    writeInt(LED_DIR + "delay_off", 100);
 }
 
-void Sensoralarm::updateSensortemp(int newsensorTemp) {
+void Sensoralarm::updateSensortemp(int newsensorTemp)
+{
     sensorTemp = newsensorTemp;
     ledStatusChange();
 }
 
-int Sensoralarm::getLedStatus() {
+int Sensoralarm::getLedStatus()
+{
     return ledStatus;
 }
 
-int Sensoralarm::getSensorTemp() {
+int Sensoralarm::getSensorTemp()
+{
     return sensorTemp;
 }
 
-void Sensoralarm::ledStatusChange() {
-
-    if(sensorTemp <= 39){
+void Sensoralarm::ledStatusChange()
+{
+    if (sensorTemp <= 39) {
         ledStatus = 0;
         setOff();
-    } else if(sensorTemp <= 59){
+    } else if (sensorTemp <= 59) {
         ledStatus = 1;
         blinkSlow();
-    } else if(sensorTemp <= 79){
+    } else if (sensorTemp <= 79) {
         ledStatus = 2;
         blinkFast();
     } else {
--- alarm/alarm.h
+++ alarm/alarm.h
@@ -8,15 +8,19 @@ public:
     void updateSensortemp(int newsensorTemp);
     int getLedStatus();
     int getSensorTemp();
+
 private:
     void ledStatusChange();
-    static constexpr const char* LED_DIR = "/sys/class/leds/am62-sk:green:heartbeat/";
     bool writeFile(const std::string& path, const std::string& value);
     bool writeInt(const std::string& path, int v);
+
     void setOff();
     void setOn();
     void blinkSlow();
     void blinkFast();
+
+    static const std::string LED_DIR;
+
     int ledStatus = 0;
     int sensorTemp = 20;
 };
